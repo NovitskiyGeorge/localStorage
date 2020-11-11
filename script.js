a38421e1faebfb5de093701f7770c1dcd0743cef
George <novitskiy.gs@gmail.com>
2020-11-11 13:01:21 +0300

    for and forEach

--- script.js
+++ script.js
@@ -38,37 +38,69 @@
 
 
 
-let text = document.querySelector('#text');
-let btn = document.querySelector('.btn');
+// let text = document.querySelector('#text');
+// let btn = document.querySelector('.btn');
 
 
-function init() {
-const STORAGE_COUNTER = 'count-number';
-let count = localStorage.getItem(STORAGE_COUNTER);
-if (count === null) {
-    localStorage.setItem(STORAGE_COUNTER, 1);
-    alert('Hello bro');
-    return;
-} else if(count % 2 == 0) {
-    alert('Hello bro');
-}
-count = Number.parseInt(count);
-count++; // count++;
-localStorage.setItem(STORAGE_COUNTER, count);
-text.value = `Hello ${localStorage.getItem(STORAGE_COUNTER)} time!` ;
-// if(count % 2 != 0 || count == 1) {
+// function init() {
+// const STORAGE_COUNTER = 'count-number';
+// let count = localStorage.getItem(STORAGE_COUNTER);
+// if (count === null) {
+//     localStorage.setItem(STORAGE_COUNTER, 1);
+//     alert('Hello bro');
+//     return;
+// } else if(count % 2 == 0) {
 //     alert('Hello bro');
 // }
+// count = Number.parseInt(count);
+// count++; // count++;
+// localStorage.setItem(STORAGE_COUNTER, count);
+// text.value = `Hello ${localStorage.getItem(STORAGE_COUNTER)} time!` ;
+// // if(count % 2 != 0 || count == 1) {
+// //     alert('Hello bro');
+// // }
 
-}
+// }
 
 
-init();
+// init();
 
-btn.addEventListener('click', function() {
-    localStorage.clear();
-    text.value = localStorage.getItem('textArea');
+// btn.addEventListener('click', function() {
+//     localStorage.clear();
+//     text.value = localStorage.getItem('textArea');
 
+// });
+
+
+
+// sam 3
+
+
+// метод for
+
+// let arr = [1, 2, 3, 4, 1234];
+// let sum = 0;
+// let multiple = 1;
+
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     multiple *= arr[i]; 
+// }
+
+// console.log(`Сумма всех элементов массива = ${sum}`);
+// console.log(`Произведение всех элементов массива = ${multiple}`);
+
+
+// метод forEach
+
+let arr = [1, 2, 3, 4, 1234];
+let sum = 0;
+let multiple = 1;
+
+arr.forEach(function(item) {
+    sum += item;
+    multiple *= item; 
 });
 
-// new branch test merge
+console.log(`Сумма всех элементов массива = ${sum}`);
+console.log(`Произведение всех элементов массива = ${multiple}`);
